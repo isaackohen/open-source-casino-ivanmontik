74ece342c65c91ea11b71c657c03ec83440999de
isaac <isaac@davidkohen.com>
2022-02-18 06:16:24 +0100

    api uncommented

--- routes/api.php
+++ routes/api.php
@@ -3,20 +3,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\Games\ThirdpartyGamesController;
-//use Spatie\QueryBuilder\QueryBuilder;
+use App\Http\Controllers\Games\ThirdpartyGamesController;
+use Spatie\QueryBuilder\QueryBuilder;
 
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
 
 
 Route::any('/callback/extGames/balance', [ThirdpartyGamesController::class, 'balance']);
 Route::any('/callback/extGames/bet', [ThirdpartyGamesController::class, 'result']);
 
+
+/*
+|--------------------------------------------------------------------------
+| Disabled API Routes
+|--------------------------------------------------------------------------
 Route::get('/gamelist', function (Request $request) {
 
 $gamelist = QueryBuilder::for(\App\Models\GamelistPublic::class)
@@ -27,5 +27,5 @@ $gamelist = QueryBuilder::for(\App\Models\GamelistPublic::class)
    return $gamelist;
 });
 
-|
+
 */
